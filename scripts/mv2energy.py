b999ed07b5ccc81e223f4b02a1ccf0dfb11a5e11
SAMBA beamline <>
2025-10-13 16:54:31 +0200

    Bender backlash active (15ksteps)

--- scripts/mv2energy.py
+++ scripts/mv2energy.py
@@ -1,4 +1,5 @@
-def mv2energy(dest=None,backlash=15000.):
+#def mv2energy(dest=None,backlash=15000.):
+def mv2energy(dest=None,backlash=0.):
     if dest == None:
         return dcm.pos()
     if dcm.pos()>dest and backlash != 0:
